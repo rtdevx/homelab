63b1b9a40403455878c072a5cb798d52a37e8b3b
rtdevx <rt.dev@onet.eu>
2025-05-04 12:42:42 +0100

    .

--- PowerShell/WindowsBootstrap.ps1
+++ PowerShell/WindowsBootstrap.ps1
@@ -300,8 +300,11 @@ Write-Host `n"Setting up Windows Terminal..."`n -ForegroundColor Green
 #Copy dotfile
 #C:\Users\$env:UserName\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState
 
-# Installing Nerd Fonts (source: https://www.nerdfonts.com/)
+#Installing Nerd Fonts (source: https://www.nerdfonts.com/)
 Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/rtdevx/homelab/refs/heads/main/PowerShell/NerdFonts-Install.ps1'))
+
+#Installing Starhip Cross-Shell Prompt to start with Windows Terminal
+Add-Content -Path $PROFILE -Value "Invoke-Expression (&starship init powershell)"
 
 ### Privacy ###
 
