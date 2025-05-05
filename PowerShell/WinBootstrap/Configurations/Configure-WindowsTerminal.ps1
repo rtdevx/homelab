a9be89a181f30c992cbe01850ad73ce406fbed08
robk <robk@localhost>
2025-05-05 21:36:26 +0100

    .

--- PowerShell/WinBootstrap/Configurations/Configure-WindowsTerminal.ps1
+++ PowerShell/WinBootstrap/Configurations/Configure-WindowsTerminal.ps1
@@ -53,6 +53,7 @@ Starship
 Write-Host `n"Applying Windows Terminal Settings."`n -ForegroundColor Green
 #Invoke-WebRequest -Uri https://raw.githubusercontent.com/rtdevx/dotfiles/refs/heads/main/terminal/WindowsTerminal.json -OutFile $env:USERPROFILE\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json
 
+<#
 # Define the path to the JSON file
 $jsonPath = "$env:USERPROFILE\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json"
 
@@ -139,3 +140,4 @@ $jsonContent.copyOnSelect = $true
 $jsonContent | ConvertTo-Json -Depth 10 | Set-Content -Path $jsonPath
 
 Write-Host "Windows Terminal settings successfully updated!"
+#>
