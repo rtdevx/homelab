1a7ef9f7c4ece8140dddfefa35bfd3ead0946caa
rtclo <rtclo@localhost>
2025-05-05 02:29:17 +0100

    .

--- PowerShell/WinBootstrap/Configurations/Configure-Privacy.ps1
+++ PowerShell/WinBootstrap/Configurations/Configure-Privacy.ps1
@@ -138,7 +138,7 @@ schtasks /Change /TN "Microsoft\Windows\CloudExperienceHost\CreateObjectTask" /d
 #schtasks /Change /TN "Microsoft\Windows\Customer Experience Improvement Program\BthSQM" /disable
 schtasks /Change /TN "Microsoft\Windows\Customer Experience Improvement Program\Consolidator" /disable
 #schtasks /Change /TN "Microsoft\Windows\Customer Experience Improvement Program\KernelCeipTask" /disable
-schtasks /Change /TN "Microsoft\Windows\Customer Experience Improvement Program\Uploader" /disable
+#schtasks /Change /TN "Microsoft\Windows\Customer Experience Improvement Program\Uploader" /disable
 schtasks /Change /TN "Microsoft\Windows\Customer Experience Improvement Program\UsbCeip" /disable
 schtasks /Change /TN "Microsoft\Windows\DiskDiagnostic\Microsoft-Windows-DiskDiagnosticDataCollector" /disable
 schtasks /Change /TN "Microsoft\Windows\DiskFootprint\Diagnostics" /disable
