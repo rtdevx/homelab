ed6ae97235a2f506549cd1b5c9843c1de2985337
rtclo <rtclo@localhost>
2025-05-05 02:23:21 +0100

    Updated Privacy settings.

--- PowerShell/WinBootstrap/Configurations/Configure-Privacy.ps1
+++ PowerShell/WinBootstrap/Configurations/Configure-Privacy.ps1
@@ -47,7 +47,7 @@ Get-AppxPackage -allusers Microsoft.549981C3F5F10 | Remove-AppxPackage
 
 #Remove Music,TV
 Get-AppxPackage -AllUsers *zune* | Remove-AppxPackage
-Get-WindowsPackage -Online | Where PackageName -like *MediaPlayer* | Remove-WindowsPackage -Online -NoRestart
+#Get-WindowsPackage -Online | Where PackageName -like *MediaPlayer* | Remove-WindowsPackage -Online -NoRestart
 
 #Remove M$ Solitaire collection
 Get-AppxPackage *Microsoft.MicrosoftSolitaireCollection* | Remove-AppxPackage
@@ -73,7 +73,7 @@ Get-AppxPackage -AllUsers *soundrec* | Remove-AppxPackage
 Get-WindowsPackage -Online | Where PackageName -like *QuickAssist* | Remove-WindowsPackage -Online -NoRestart
 
 #Remove XBOX
-Get-AppxPackage -AllUsers *xbox* | Remove-AppxPackage
+#Get-AppxPackage -AllUsers *xbox* | Remove-AppxPackage
 sc delete XblAuthManager
 sc delete XblGameSave
 sc delete XboxNetApiSvc
@@ -95,7 +95,7 @@ sc delete WerSvc
 sc delete MessagingService
 sc delete wercplsupport
 sc delete PcaSvc
-sc config wlidsvc start=demand
+#sc config wlidsvc start=demand
 sc delete wisvc
 sc delete RetailDemo
 sc delete diagsvc
@@ -128,16 +128,16 @@ reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution 
 reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\DeviceCensus.exe" /v Debugger /t REG_SZ /d "%windir%\System32\taskkill.exe" /f
 
 #Disabling Privacy-Related Scheduled Tasks
-schtasks /Change /TN "Microsoft\Windows\AppID\SmartScreenSpecific" /disable
-schtasks /Change /TN "Microsoft\Windows\Application Experience\AitAgent" /disable
+#schtasks /Change /TN "Microsoft\Windows\AppID\SmartScreenSpecific" /disable
+#schtasks /Change /TN "Microsoft\Windows\Application Experience\AitAgent" /disable
 schtasks /Change /TN "Microsoft\Windows\Application Experience\Microsoft Compatibility Appraiser" /disable
-schtasks /Change /TN "Microsoft\Windows\Application Experience\ProgramDataUpdater" /disable
+#schtasks /Change /TN "Microsoft\Windows\Application Experience\ProgramDataUpdater" /disable
 schtasks /Change /TN "Microsoft\Windows\Application Experience\StartupAppTask" /disable
 schtasks /Change /TN "Microsoft\Windows\Autochk\Proxy" /disable
 schtasks /Change /TN "Microsoft\Windows\CloudExperienceHost\CreateObjectTask" /disable
-schtasks /Change /TN "Microsoft\Windows\Customer Experience Improvement Program\BthSQM" /disable
+#schtasks /Change /TN "Microsoft\Windows\Customer Experience Improvement Program\BthSQM" /disable
 schtasks /Change /TN "Microsoft\Windows\Customer Experience Improvement Program\Consolidator" /disable
-schtasks /Change /TN "Microsoft\Windows\Customer Experience Improvement Program\KernelCeipTask" /disable
+#schtasks /Change /TN "Microsoft\Windows\Customer Experience Improvement Program\KernelCeipTask" /disable
 schtasks /Change /TN "Microsoft\Windows\Customer Experience Improvement Program\Uploader" /disable
 schtasks /Change /TN "Microsoft\Windows\Customer Experience Improvement Program\UsbCeip" /disable
 schtasks /Change /TN "Microsoft\Windows\DiskDiagnostic\Microsoft-Windows-DiskDiagnosticDataCollector" /disable
@@ -150,7 +150,7 @@ schtasks /Change /TN "Microsoft\Windows\Shell\FamilySafetyMonitor" /disable
 schtasks /Change /TN "Microsoft\Windows\Shell\FamilySafetyRefresh" /disable
 schtasks /Change /TN "Microsoft\Windows\Shell\FamilySafetyUpload" /disable
 schtasks /Change /TN "Microsoft\Windows\Windows Error Reporting\QueueReporting" /disable
-schtasks /Change /TN "Microsoft\Windows\WindowsUpdate\Automatic App Update" /disable
+#schtasks /Change /TN "Microsoft\Windows\WindowsUpdate\Automatic App Update" /disable
 schtasks /Change /TN "Microsoft\Windows\License Manager\TempSignedLicenseExchange" /disable
 schtasks /Change /TN "Microsoft\Windows\Clip\License Validation" /disable
 schtasks /Change /TN "\Microsoft\Windows\ApplicationData\DsSvcCleanup" /disable
@@ -158,11 +158,10 @@ schtasks /Change /TN "\Microsoft\Windows\Power Efficiency Diagnostics\AnalyzeSys
 schtasks /Change /TN "\Microsoft\Windows\PushToInstall\LoginCheck" /disable
 schtasks /Change /TN "\Microsoft\Windows\PushToInstall\Registration" /disable
 schtasks /Change /TN "\Microsoft\Windows\Shell\FamilySafetyMonitor" /disable
-schtasks /Change /TN "\Microsoft\Windows\Shell\FamilySafetyMonitorToastTask" /disable
+#schtasks /Change /TN "\Microsoft\Windows\Shell\FamilySafetyMonitorToastTask" /disable
 schtasks /Change /TN "\Microsoft\Windows\Shell\FamilySafetyRefreshTask" /disable
 schtasks /Change /TN "\Microsoft\Windows\Subscription\EnableLicenseAcquisition" /disable
 schtasks /Change /TN "\Microsoft\Windows\Subscription\LicenseAcquisition" /disable
 schtasks /Change /TN "\Microsoft\Windows\Diagnosis\RecommendedTroubleshootingScanner" /disable
 schtasks /Change /TN "\Microsoft\Windows\Diagnosis\Scheduled" /disable
-schtasks /Change /TN "\Microsoft\Windows\NetTrace\GatherNetworkInfo" /disable
-del /F /Q "C:\Windows\System32\Tasks\Microsoft\Windows\SettingSync\*" 
+#schtasks /Change /TN "\Microsoft\Windows\NetTrace\GatherNetworkInfo" /disable
