3ffccc23d324cb77beda3585947a7e5c91824002
robk <rt.dev@onet.eu>
2025-06-07 23:25:22 +0000

    Added Hugo static site generator to installers

--- PowerShell/WinBootstrap/WinBootstrap.ps1
+++ PowerShell/WinBootstrap/WinBootstrap.ps1
@@ -78,16 +78,16 @@ $apps = @(
     @{name = "WinDirStat.WinDirStat" },
     @{name = "Google.Chrome" }, 
     @{name = "Mozilla.Firefox" }, 
-#    @{name = "TradingView.TradingViewDesktop" },
     @{name = "QNAP.Qsync" },
     @{name = "Garmin.Express" },
     @{name = "mRemoteNG.mRemoteNG" },
     @{name = "Lenovo.SystemUpdate" },
     @{name = "GNU.Wget2" },
     @{name = "Obsidian.Obsidian" },
+    @{name = "Hugo.Hugo" },    
     @{name = "Zoom.Zoom" },    
     # Starthip Cross-Shell Prompt for Windows Terminal (https://starship.rs/)
-    @{name = "Starship.Starship" }    
+    @{name = "Starship.Starship" }
 );
 
 #More packages: https://winget.run/
