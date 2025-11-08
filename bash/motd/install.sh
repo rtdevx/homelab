77b50af7a6c2fac04304c2f1caf6f63511af6103
robk <robk@localhost>
2025-11-08 01:40:45 +0000

    .

--- bash/motd/install.sh
+++ bash/motd/install.sh
@@ -2,7 +2,7 @@
 # Execute: curl https://raw.githubusercontent.com/rtdevx/homelab/refs/heads/main/bash/motd/install.sh | bash
 
 sudo chmod -x /etc/update-motd.d/*
-sudo apt update && sudo apt install inxi screenfetch ansiweather
+sudo apt update && sudo apt upgrade -y && sudo apt install -y inxi screenfetch ansiweather
 
 wget -O /etc/update-motd.d/01-custom https://raw.githubusercontent.com/rtdevx/homelab/refs/heads/main/bash/motd/01-custom
 
