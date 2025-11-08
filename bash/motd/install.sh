1158ac82874592750328704c3b101a01dfbc0ebc
robk <robk@localhost>
2025-11-08 02:01:20 +0000

    .

--- bash/motd/install.sh
+++ bash/motd/install.sh
@@ -5,7 +5,7 @@
 sudo chmod -x /etc/update-motd.d/*
 
 # Install prerequisites
-sudo apt update && sudo apt upgrade -y && sudo apt install -y inxi screenfetch ansiweather
+sudo apt update && sudo apt upgrade -y && sudo apt install -y inxi screenfetch ansiweather show-motd
 
 # Download custom MOTD script
 sudo wget -O /etc/update-motd.d/01-custom https://raw.githubusercontent.com/rtdevx/homelab/refs/heads/main/bash/motd/01-custom
