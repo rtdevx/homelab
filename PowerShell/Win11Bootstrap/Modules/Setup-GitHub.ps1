dc78ab7084968f8a55913bd5d0add6be4de42d3d
Rob <47715438+rtdevx@users.noreply.github.com>
2026-01-01 20:32:49 +0000

    Update Setup-GitHub.ps1

--- PowerShell/Win11Bootstrap/Modules/Setup-GitHub.ps1
+++ PowerShell/Win11Bootstrap/Modules/Setup-GitHub.ps1
@@ -156,7 +156,7 @@ if (-not $alreadyUploaded) {
 # ------------------------------------------------------------
 
 $sshConfigPath = Join-Path $sshPath "config"
-$sshConfigUrl  = "https://raw.githubusercontent.com/rtdevx/dotfiles/refs/heads/main/ssh/config"
+$sshConfigUrl = "https://raw.githubusercontent.com/rtdevx/dotfiles/main/ssh/config"
 
 Write-Host "Downloading SSH config from $sshConfigUrl to $sshConfigPath..."
 
