b05ea32f3056e5a64b8498a82d00bed3e0d2e5cf
robk <robk@localhost>
2026-01-24 10:55:37 +0000

    [HL-51] Reorganizing Repositories

--- PowerShell/Win11Bootstrap/_ARCHIVE/WinBootstrap/Configurations/Update-WingetPackages.ps1
+++
@@ -1,6 +0,0 @@
-Start-Transcript -Path $LogsFolder\Update-WingetPackages.log
-
-Write-Host `n"Updating Winget Packages."`n -ForegroundColor Green
-winget source update ; winget upgrade --all --include-unknown --accept-package-agreements --accept-source-agreements --silent
-
-Stop-Transcript
