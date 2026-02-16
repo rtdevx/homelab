52293698c45694234cbabfb424c06c81b6a3b030
robk <johndoe@example.com>
2026-02-16 12:52:28 +0000

    .

--- bash/examples/simple_log_parser/main.sh
+++ bash/examples/simple_log_parser/main.sh
@@ -22,6 +22,8 @@ for log_file in ${log_files[@]}; do
             pattern_count=$(sudo grep -c "$pattern" "$log_file")
             
             # Only show errors if they exist
+            # ! "sudo grep" will always write to /var/log/auth.log. Those are false positives and indicate sudo execution. Consider removing auth.log from the script (below line) and use separate script for auth.log?
+            #if [[ $pattern_count -gt 0 && $log_file != "/var/log/auth.log" ]]; then            
             if [[ $pattern_count -gt 0 ]]; then
 
                 echo -e "\n"
@@ -29,8 +31,7 @@ for log_file in ${log_files[@]}; do
                 echo "FILE: $log_file ($pattern: $pattern_count)"
                 echo "=========================================================="
 
-                # ! "sudo grep" will always write to /var/log/auth.log. Those are false positives and indicate sudo execution.
-                sudo grep "$pattern" "$log_file"
+                sudo grep -i "$pattern" "$log_file"
             
             fi
 
